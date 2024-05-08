1301a3e4edacbce9a518ad5367e018f96d42db7d
nachoiborraIES <nachoiborra@iessanvicente.com>
2024-05-08 17:19:01 +0200

    jquery

--- js/app.js
+++ js/app.js
@@ -16,22 +16,16 @@ function mostrar(id)
 function cargar(div, id_doc)
 {
     alert(id_doc);
+    var resultado = null;
+    $.ajax( { url: 'md/' + id_doc + '.md', 
+        type: 'get', 
+        dataType: 'html',
+        async: false,
+        success: function(data) { resultado = data; } 
+    });
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'md/' + id_doc + '.md', true);
-    xhr.onreadystatechange = function () {
-      if(xhr.readyState === 4 && xhr.status === 200) {
-        alert("OK");
-        var converter = new showdown.Converter(),  // Utilizamos Showdown para convertir Markdown a HTML
-            markdownText = xhr.responseText,
-            html = converter.makeHtml(markdownText);
-        document.getElementById(div).innerHTML = html;
-      }
-      else
-      {
-        alert(xhr.readyState);
-        alert(xhr.status);
-      }
-    };
-    xhr.send();    
+    resultado = markdown.toHTML( resultado );
+    document.getElementById(div).innerHTML = resultado;
+
+    alert("Fin");
 }
