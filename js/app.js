aa9580ba01fd8983ebe0035ebc4967da78ae0a3f
nachoiborraIES <nachoiborra@iessanvicente.com>
2024-05-08 17:02:28 +0200

    Ajax MD

--- js/app.js
+++ js/app.js
@@ -16,4 +16,16 @@ function mostrar(id)
 function cargar(div, id_doc)
 {
     alert(id_doc);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/md/' + id_doc + '.md', true);
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === 4 && xhr.status === 200) {
+        var converter = new showdown.Converter(),  // Utilizamos Showdown para convertir Markdown a HTML
+            markdownText = xhr.responseText,
+            html = converter.makeHtml(markdownText);
+        document.getElementById(div).innerHTML = html;
+      }
+    };
+    xhr.send();    
 }
